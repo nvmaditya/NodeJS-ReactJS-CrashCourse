2038b95b9e9d03ed7db9f4a3908f03b015ff302e
AdixSasuke <adityakhandelwal0271@gmail.com>
2025-10-15 16:48:30 +0530

    Add Day1 Next.js project with demo pages and configure gitignore

---
+++ Day1/srmwsnext/src/app/another_demo/yet_another_demo/page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <h1 className="flex justify-center h-screen items-center">
+            Yet Another Demo
+        </h1>
+    );
+}
---
+++ Day1/srmwsnext/src/app/demo/page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <h1 className="flex justify-center h-screen items-center">
+            Hello Next.js!
+        </h1>
+    );
+}
